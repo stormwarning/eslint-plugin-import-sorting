c8975e5e37cbd58a24d7a3adf8dcebe86c7849f6
Jeff <rustyangel@gmail.com>
2019-11-14 14:14:49 -0700

    Chore: Add Prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSpacing: true,
+    proseWrap: 'never',
+    semi: false,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'es5',
+}
