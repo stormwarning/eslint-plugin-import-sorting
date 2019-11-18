37ee3c3792ba116a7fb597739c95de67db57dd06
Jeff <rustyangel@gmail.com>
2019-11-17 22:16:24 -0700

    Chore: Add linter configs for eslint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
-    extends: '@zazen/eslint-config',
-    rules: {},
+    plugins: ['eslint-plugin', 'filenames', 'import', 'isort', 'node'],
+    extends: [
+        '@zazen/eslint-config',
+        'plugin:eslint-comments/recommended',
+        'plugin:eslint-plugin/all',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:node/recommended',
+    ],
+    rules: {
+        'isort/order': 'error',
+    },
 }
