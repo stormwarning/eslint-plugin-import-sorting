f3b5c409ac0ec0e257e1c7dc7ea82abe03222bc8
Jeff <rustyangel@gmail.com>
2019-11-22 13:46:54 -0700

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,39 @@
 module.exports = {
-    plugins: ['eslint-plugin', 'filenames', 'import', 'isort', 'node'],
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        // ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    env: {
+        node: true,
+        es6: true,
+    },
+    plugins: [
+        '@typescript-eslint',
+        'eslint-plugin',
+        'filenames',
+        // 'import',
+        'isort',
+        // 'node',
+    ],
     extends: [
         '@zazen/eslint-config',
+        'plugin:@typescript-eslint/recommended',
         'plugin:eslint-comments/recommended',
         'plugin:eslint-plugin/all',
-        'plugin:import/errors',
-        'plugin:import/warnings',
-        'plugin:node/recommended',
+        // 'plugin:import/errors',
+        // 'plugin:import/warnings',
+        // 'plugin:node/recommended',
     ],
     rules: {
+        'prefer-const': 'off',
+
+        '@typescript-eslint/member-delimiter-style': [
+            'error',
+            { multiline: { delimiter: 'none' } },
+        ],
+
         'isort/order': 'error',
     },
 }
